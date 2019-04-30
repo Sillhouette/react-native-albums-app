47d03f34bf05ec4a3e7ca22395395777c17de264
github username <raustin.melchior@gmail.com>
2019-04-30 11:27:50 -0700

    add styling for image and text in CardDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
-const AlbumDetail = props => (
-  <Card>
-    <CardItem>
-      <Text>Title: {props.album.title}</Text>
-      <Text>Artist: {props.album.artist}</Text>
-    </CardItem>
-  </Card>
-);
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyle, headerContentStyle } = styles;
+
+  return (
+    <Card>
+      <CardItem>
+        <View>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+
+        <View style={headerContentStyle}>
+          <Text>Title: {title}</Text>
+          <Text>Artist: {artist}</Text>
+        </View>
+      </CardItem>
+    </Card>
+  );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+};
 
 export default AlbumDetail;
