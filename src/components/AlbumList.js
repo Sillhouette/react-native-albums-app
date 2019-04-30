376b408cfcbec8fd0831747fa21552324adbaeb5
github username <raustin.melchior@gmail.com>
2019-04-30 09:33:19 -0700

    add state to AlbumList and add fetch request for data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  state = {
+    albums: []
+  };
+
+  componentWillMount() {
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response =>
+      this.setState({
+        albums: response.data
+      })
+    );
+  }
+
   render() {
+    console.log(this.state);
+
     return (
       <View>
         <Text>Album List!</Text>
